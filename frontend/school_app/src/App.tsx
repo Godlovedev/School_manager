591e0c3af841d57f1dc42389dc4108fb8151006f
Godlovedev <kengnegodlove@gmail.com>
2025-06-26 11:02:14 +0100

    initialisation du projet

---
+++ frontend/school_app/src/App.tsx
@@ -0,0 +1,5 @@
+export default function App(){
+  return <div className="flex h-screen items-center justify-center">
+    <p className="bg-zinc-200 shadow-2xl rounded-lg p-3">Debut du projet</p>
+  </div>
+}

e3f940e5af418f2852b0bbb5ccf204a2abab6797
Godlovedev <kengnegodlove@gmail.com>
2025-06-30 09:13:47 +0100

    mise un place du serializer du User

---
+++ backend/accounts/serializers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the User model.
+    This serializer is used to convert User instances into JSON format and vice versa.
+    """
+    password = serializers.CharField(write_only=True, required=True, min_length=6)
+    
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        read_only_fields = ['id']
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("Ce nom d'utilisateur est déjà pris")
+        return value
+    
+    def validate_password(self, value):
+        if len(value) < 6:
+            raise serializers.ValidationError("Le mot de passe doit contenir au moins 6 caractères")
+        return value

327d6ff1695d2319dc569a251c05443c169ce78d
Godlovedev <kengnegodlove@gmail.com>
2025-07-04 18:40:28 +0100

    gestion des operation de crud sur les ecoles

--- backend/schools/urls.py
+++ backend/schools/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ListSchoolsView
+from .views import ListSchoolsView, CreateSchoolsView, DeleteSchoolsView, GetSchoolView, UpdateSchoolView
 
 urlpatterns = [
     path("", ListSchoolsView.as_view(), name="list_schools"),
+    path("create/", CreateSchoolsView.as_view(), name="create_school"),
+    path("delete/<int:pk>/", DeleteSchoolsView.as_view(), name="delete_school"),
+    path("<int:pk>/", GetSchoolView.as_view(), name="get_school"),
+    path("<int:pk>/update/", UpdateSchoolView.as_view(), name="update_school"),
 ]
